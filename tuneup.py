a525e507c57d04194ede166bfb61c08f63c3bc6d
Kevin Blount <kvbchamp@gmail.com>
2020-06-30 11:45:42 -0400

    completed cProfile decorator function

--- tuneup.py
+++ tuneup.py
@@ -10,6 +10,7 @@ __author__ = "Kevin Blount"
 import cProfile
 import pstats
 import functools
+import io
 import timeit
 
 
@@ -17,9 +18,19 @@ def profile(func):
     """A cProfile decorator function that can be used to
     measure performance.
     """
-    # Be sure to review the lesson material on decorators.
-    # You need to understand how they are constructed and used.
-    raise NotImplementedError("Complete this decorator function")
+    def inner(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        retrv = func(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retrv
+
+    return inner
 
 
 def read_movies(src):
@@ -37,6 +48,7 @@ def is_duplicate(title, movies):
     return False
 
 
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list."""
     movies = read_movies(src)
@@ -51,14 +63,20 @@ def find_duplicate_movies(src):
 # TODO - use timeit to test the main() function. Replace stmt with needed statment
 def timeit_helper():
     """Part A: Obtain some profiling measurements using timeit."""
+    avg_results = []
     t = timeit.Timer(stmt='pass', setup='pass')
     result = sorted(t.repeat(repeat=7, number=3))
+    for float_num in result:
+        avg_result = float_num / 3
+        avg_results.append(avg_result)
     print('Best time across 7 repeats of 5 runs per repeat: {:.11} sec'.format(
-        min(result)))
+        min(avg_results)))
 
 
+@profile
 def main():
     """Computes a list of duplicate movie entries."""
+    timeit_helper()
     result = find_duplicate_movies('movies.txt')
     print(f'Found {len(result)} duplicate movies:')
     print('\n'.join(result))
